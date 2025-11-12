e1a3cf0578ae668fd3d1cbef3d32f5e28919dcbf
WANG BOYU <wby1279501447@gmail.com>
2025-11-12 13:32:32 +0800

    Update cs3237_integrated_latest.ino

--- cs3237_integrated_latest/cs3237_integrated_latest.ino
+++ cs3237_integrated_latest/cs3237_integrated_latest.ino
@@ -31,9 +31,9 @@
 
 // --- ADDED: WiFi & MQTT ---
 // !!! FILL IN YOUR DETAILS HERE !!!
-const char* ssid = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
-const char* mqttServer = "YOUR_MQTT_BROKER_IP"; // e.g., "192.168.1.100"
+const char* ssid = "CPLADRAGON";
+const char* password = "10293847";
+const char* mqttServer = "178.128.94.113"; // e.g., "192.168.1.100"
 const int mqttPort = 1883;
 const char* mqttTopic = "esp32/sensor_data"; // Topic to publish to
 // ---
